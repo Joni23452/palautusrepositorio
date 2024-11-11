88c7efd790f1feab3be562f27badadebb7b77a16
Joni <joni.kaup@outlook.com>
2024-11-11 22:59:00 +0200

    Week 2 exercise 12 refactoring

--- viikko2/nhl-reader/src/index.py
+++ viikko2/nhl-reader/src/index.py
@@ -1,36 +1,41 @@
 import requests
 from player import Player
 
+class PlayerReader:
+    def __init__(self, url):
+        self.response = requests.get(url).json()
+
+    def get_response(self):
+        return self.response
+
+class PlayerStats:
+    def __init__(self, reader):
+        self.stats = reader.get_response()
+        self.players = []
+        for player_dict in self.stats:
+            player = Player(player_dict)
+            self.players.append(player)
+
+    def get_stats(self):
+        return self.stats
+    
+    def top_scorers_by_nationality(self, nationality):
+        scorers_list = []
+        for player in self.players:
+            if player.get_nationality() == nationality:
+                scorers_list.append((player, player.assists_plus_goals()))
+        scorers_list.sort(key=lambda x: x[1], reverse=True) #https://stackoverflow.com/a/8459243
+
+        return scorers_list
+
 def main():
     url = "https://studies.cs.helsinki.fi/nhlstats/2023-24/players"
-    response = requests.get(url).json()
+    reader = PlayerReader(url)
+    stats = PlayerStats(reader)
+    players = stats.top_scorers_by_nationality("FIN")
 
-    print("JSON-muotoinen vastaus:")
-    #print(response)
-
-    players = []
-
-    for player_dict in response:
-        player = Player(player_dict)
-        players.append(player)
-
-    #Tehtävä 10
-    '''print("Oliot:")
-
-    for player in players:
-        if player.get_nationality() == 'FIN':
-            print(player)'''
-
-    # Tehtävä 11
-    print("Pelaajat järjestyksessä")
-    pelaajalista = []
-    for player in players:
-        if player.get_nationality() == 'FIN':
-            pelaajalista.append((player, player.assists_plus_goals()))
-    pelaajalista.sort(key=lambda x: x[1], reverse=True) #https://stackoverflow.com/a/8459243
-    
-    for pelaajatuple in pelaajalista:
-        print(f"{pelaajatuple[0]}, total {pelaajatuple[1]} points")
+    for scorers_tuple in players:
+            print(f"{scorers_tuple[0]}, total {scorers_tuple[1]} points")
 
 if __name__ == "__main__":
     main()
