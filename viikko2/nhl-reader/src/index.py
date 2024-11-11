b24d9e6d0a2816800257e598928b1eae106745c6
Joni <joni.kaup@outlook.com>
2024-11-11 22:37:15 +0200

    Week 2 exercise 11 improved player list

--- viikko2/nhl-reader/src/index.py
+++ viikko2/nhl-reader/src/index.py
@@ -14,12 +14,23 @@ def main():
         player = Player(player_dict)
         players.append(player)
 
-    print("Oliot:")
+    #Tehtävä 10
+    '''print("Oliot:")
 
     for player in players:
         if player.get_nationality() == 'FIN':
-            print(player)
+            print(player)'''
 
+    # Tehtävä 11
+    print("Pelaajat järjestyksessä")
+    pelaajalista = []
+    for player in players:
+        if player.get_nationality() == 'FIN':
+            pelaajalista.append((player, player.assists_plus_goals()))
+    pelaajalista.sort(key=lambda x: x[1], reverse=True) #https://stackoverflow.com/a/8459243
+    
+    for pelaajatuple in pelaajalista:
+        print(f"{pelaajatuple[0]}, total {pelaajatuple[1]} points")
 
 if __name__ == "__main__":
     main()
--- viikko2/nhl-reader/src/player.py
+++ viikko2/nhl-reader/src/player.py
@@ -8,6 +8,15 @@ class Player:
     
     def get_nationality(self):
         return self.nationality
+    
+    def get_goals(self):
+        return self.goals
+    
+    def get_assists(self):
+        return self.assists
+    
+    def assists_plus_goals(self):
+        return self.goals + self.assists
 
     def __str__(self):
         return f"{self.name} team {self.team} | goals {self.goals}, assists {self.assists}"
