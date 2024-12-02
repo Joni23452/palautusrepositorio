6348da15aa902d4ac8c4b35a84e8e66882e7a3dd
Joni <joni.kaup@outlook.com>
2024-12-02 20:34:13 +0200

    Added Player class for tracking player scores

--- viikko5/tennis/src/tennis_game.py
+++ viikko5/tennis/src/tennis_game.py
@@ -1,31 +1,43 @@
+class Player:
+    def __init__(self, name):
+        self.name = name
+        self.score = 0
+
+    def add_point(self):
+        self.score += 1
+
+    def get_name(self):
+        return self.name
+    
+    def get_player_score(self):
+        return self.score
+
 class TennisGame:
     def __init__(self, player1_name, player2_name):
-        self.player1_name = player1_name
-        self.player2_name = player2_name
-        self.player1_score = 0
-        self.player2_score = 0
+        self.player1 = Player(player1_name)
+        self.player2 = Player(player2_name)
 
     def won_point(self, player_name):
-        if player_name == self.player1_name:
-            self.player1_score += 1
-        elif player_name == self.player2_name:
-                self.player2_score += 1
+        if player_name == self.player1.get_name():
+            self.player1.add_point()
+        elif player_name == self.player2.get_name():
+            self.player2.add_point()
 
     def get_score(self):
         score = ""
         temp_score = 0
 
-        if self.player1_score == self.player2_score:
-            if self.player1_score == 0:
+        if self.player1.get_player_score() == self.player2.get_player_score():
+            if self.player1.get_player_score() == 0:
                 score = "Love-All"
-            elif self.player1_score == 1:
+            elif self.player1.get_player_score() == 1:
                 score = "Fifteen-All"
-            elif self.player1_score == 2:
+            elif self.player1.get_player_score() == 2:
                 score = "Thirty-All"
             else:
                 score = "Deuce"
-        elif self.player1_score >= 4 or self.player2_score >= 4:
-            minus_result = self.player1_score - self.player2_score
+        elif self.player1.get_player_score() >= 4 or self.player2.get_player_score() >= 4:
+            minus_result = self.player1.get_player_score() - self.player2.get_player_score()
 
             if minus_result == 1:
                 score = "Advantage player1"
@@ -38,10 +50,10 @@ class TennisGame:
         else:
             for i in range(1, 3):
                 if i == 1:
-                    temp_score = self.player1_score
+                    temp_score = self.player1.get_player_score()
                 else:
                     score = score + "-"
-                    temp_score = self.player2_score
+                    temp_score = self.player2.get_player_score()
 
                 if temp_score == 0:
                     score = score + "Love"
