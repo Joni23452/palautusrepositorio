3329acf8ccafd165cda0d9332141c05a632b64b4
Joni <joni.kaup@outlook.com>
2024-12-02 20:21:19 +0200

    refactoring won_point

--- viikko5/tennis/src/tennis_game.py
+++ viikko5/tennis/src/tennis_game.py
@@ -6,10 +6,10 @@ class TennisGame:
         self.player2_score = 0
 
     def won_point(self, player_name):
-        if player_name == "player1":
-            self.player1_score = self.player1_score + 1
-        else:
-            self.player2_score = self.player2_score + 1
+        if player_name == self.player1_name:
+            self.player1_score += 1
+        elif player_name == self.player2_name:
+                self.player2_score += 1
 
     def get_score(self):
         score = ""
