a0aa527360b000333b07752f66d9af71952259b1
Joni <joni.kaup@outlook.com>
2024-12-02 21:34:21 +0200

    Moved helper functions out of class TennisGame

--- viikko5/tennis/src/tennis_game.py
+++ viikko5/tennis/src/tennis_game.py
@@ -23,54 +23,54 @@ class TennisGame:
         elif player_name == self.player2.get_name():
             self.player2.add_point()
 
-    def scores_equal(self, score1, score2):
-        return score1 == score2
-
-    def tied_at(self, score):
-        if score == 0:
-            return "Love-All"
-        elif score == 1:
-            return "Fifteen-All"
-        elif score == 2:
-            return "Thirty-All"
-        else:
-            return "Deuce"
-        
-    def in_overtime(self, score1, score2):
-        return score1 >= 4 or score2 >= 4
-    
-    def overtime_score(self, score1, score2):
-        score_difference = score1 - score2
-
-        if score_difference == 1:
-            return "Advantage player1"
-        elif score_difference == -1:
-            return "Advantage player2"
-        elif score_difference >= 2:
-            return "Win for player1"
-        else:
-            return "Win for player2"
-        
-    def player_score_to_text(self, score):
-        if score == 0:
-            return "Love"
-        elif score == 1:
-            return "Fifteen"
-        elif score == 2:
-            return "Thirty"
-        elif score == 3:
-            return "Forty"
-        
-    def game_score_to_text(self, score1, score2):
-        if self.scores_equal(score1, score2):
-            return self.tied_at(score1)
-        elif self.in_overtime(score1, score2):
-            return self.overtime_score(score1, score2)
-        else:
-            return self.player_score_to_text(score1) + "-" + self.player_score_to_text(score2)
-
     def get_score(self):
         player1_score = self.player1.get_player_score()
         player2_score = self.player2.get_player_score()
-        game_score = self.game_score_to_text(player1_score, player2_score)
+        game_score = game_score_to_text(player1_score, player2_score)
         return game_score
+    
+def scores_equal(score1, score2):
+    return score1 == score2
+
+def tied_at(score):
+    if score == 0:
+        return "Love-All"
+    elif score == 1:
+        return "Fifteen-All"
+    elif score == 2:
+        return "Thirty-All"
+    else:
+        return "Deuce"
+    
+def in_overtime(score1, score2):
+    return score1 >= 4 or score2 >= 4
+
+def overtime_score(score1, score2):
+    score_difference = score1 - score2
+
+    if score_difference == 1:
+        return "Advantage player1"
+    elif score_difference == -1:
+        return "Advantage player2"
+    elif score_difference >= 2:
+        return "Win for player1"
+    else:
+        return "Win for player2"
+    
+def player_score_to_text(score):
+    if score == 0:
+        return "Love"
+    elif score == 1:
+        return "Fifteen"
+    elif score == 2:
+        return "Thirty"
+    elif score == 3:
+        return "Forty"
+    
+def game_score_to_text(score1, score2):
+    if scores_equal(score1, score2):
+        return tied_at(score1)
+    elif in_overtime(score1, score2):
+        return overtime_score(score1, score2)
+    else:
+        return player_score_to_text(score1) + "-" + player_score_to_text(score2)
